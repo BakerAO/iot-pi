72139071ebef0536ca348ba7fd2ff4822289f9f6
BakerAO <dpshunter@gmail.com>
2019-11-09 14:36:57 -0600

    Update

--- serialCom.py
+++ serialCom.py
@@ -1,6 +1,10 @@
 import serial
 
-port = serial.Serial('/dev/cu.SLAB_USBtoUART', 115200, 8, 'N', 1, 1)
+port = serial.Serial(
+  # '/dev/cu.SLAB_USBtoUART',
+  '/dev/ttyUSB0'
+  115200, 8, 'N', 1, 1
+)
 
 while True:
   rcv = port.readline()
