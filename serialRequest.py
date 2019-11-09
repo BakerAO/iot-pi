e4068b9c31462abd6e9463e917fc97240a1723e8
BakerAO <dpshunter@gmail.com>
2019-11-09 15:05:45 -0600

    Update

---
+++ serialRequest.py
@@ -0,0 +1,18 @@
+import serial
+import requests
+
+port = serial.Serial(
+  '/dev/cu.SLAB_USBtoUART',
+  # '/dev/ttyUSB0',
+  115200, 8, 'N', 1, 1
+)
+
+while True:
+  rcv = port.readline()
+  if len(rcv) > 1:
+    url = "https://api.github.com/events"
+    response = requests.get(url)
+    print(response.status_code)
+    # print(response.text)
+    # print(response.json())
+    print(rcv)
