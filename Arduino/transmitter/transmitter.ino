afecb78b9b616fe7110201b6a26aa0a9912e446c
BakerAO <baker.ousley@gmail.com>
2020-02-29 13:47:23 -0600

    Update

--- Arduino/transmitter/transmitter.ino
+++ Arduino/transmitter/transmitter.ino
@@ -1,17 +1,29 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <DHT.h>
  
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 #define RF95_FREQ 915.0
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-int16_t packetnum = 0;
+#define DHTPIN 12
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+unsigned long lastTrans;
+
+String readSensor() {
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  String reading = "t-" + String(t);
+  reading += ", h-" + String(h);
+  return reading;
+}
  
-void setup() 
-{
+void setup() {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
+  dht.begin();
   Serial.begin(115200);
   delay(100);
   digitalWrite(RFM95_RST, LOW);
@@ -24,44 +36,28 @@ void setup()
   rf95.setTxPower(23, false);
 }
 
-void loop()
-{
-  delay(3000);
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending ");
-  Serial.println(radiopacket);
-  radiopacket[19] = 0;
+void loop() {
+  if (millis() > lastTrans + 5000) {
+    String message = readSensor();
+    Serial.println(message);
 
-  delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
- 
-  Serial.println("Waiting for packet to complete..."); 
-  delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
- 
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
+    char radiopacket[20] = "Hello World #      ";
+    Serial.println(radiopacket);
+    radiopacket[19] = 0;
+    delay(10);
+    rf95.send((uint8_t *)radiopacket, 20);
+    delay(10);
+    rf95.waitPacketSent();
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.waitAvailableTimeout(1000)) { 
+      if (rf95.recv(buf, &len)) {
+        Serial.print("Got reply: ");
+        Serial.println((char*)buf);
+        Serial.print("RSSI: ");
+        Serial.println(rf95.lastRssi(), DEC);    
+      }
     }
-    else
-    {
-      Serial.println("Receive failed");
-    }
+    lastTrans = millis();
   }
-  else
-  {
-    Serial.println("No reply, is there a listener around?");
-  }
- 
 }
