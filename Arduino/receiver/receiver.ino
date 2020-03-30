4d5ea16cf04f001ebff494b3299ec09a98c7eaa5
BakerAO <baker.ousley@gmail.com>
2020-03-30 08:11:36 -0500

    Update

--- Arduino/receiver/receiver.ino
+++ Arduino/receiver/receiver.ino
@@ -1,55 +1,64 @@
 #include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_HX8357.h"
 #include <RH_RF95.h>
 
-#define LED 13
+#define STMPE_CS 6
+#define TFT_CS   9
+#define TFT_DC   10
+#define SD_CS    5
+#define TFT_RST -1
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 #define RF95_FREQ 915.0
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-void setup()
-{
-  pinMode(LED, OUTPUT);
+void setup() {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-  Serial.begin(115200);
   delay(100);
   digitalWrite(RFM95_RST, LOW);
-  delay(10);
+  delay(100);
   digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+  delay(100);
+
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(HX8357_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(HX8357_WHITE);
+  tft.setTextSize(5);
+  tft.println("Innov8 IoT");
+  for (int i = 50; i < 56; i++) {
+    // xStart, yStart, xEnd, yEnd, color
+    tft.drawLine(0, i, 500, i, HX8357_WHITE);
+  }
+  tft.setTextSize(3);
+  tft.println("");
+}
  
+void loop() {
   rf95.init();
   rf95.setFrequency(RF95_FREQ);
   rf95.setTxPower(23, false);
-}
- 
-void loop()
-{
-  if (rf95.available())
-  {
+  if (rf95.available()) {
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
- 
-    if (rf95.recv(buf, &len))
-    {
-      digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-
-      uint8_t data[] = "And hello back to you";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      Serial.println("Sent a reply");
-      digitalWrite(LED, LOW);
-    }
-    else
-    {
-      Serial.println("Receive failed");
+    if (rf95.recv(buf, &len)) {
+      String message = (char*)buf;
+      displayText(message);
     }
   }
+  delay(1000);
+}
+
+void displayText(String message) {
+  for (int i = 100; i < 250; i++) {
+    tft.drawLine(0, i, 500, i, HX8357_BLACK);
+  }
+  tft.setCursor(0, 100);
+  tft.println(message);
 }
--- Arduino/transmitter/transmitter.ino
+++ Arduino/transmitter/transmitter.ino
@@ -15,8 +15,8 @@ unsigned long lastTrans;
 String readSensor() {
   float t = dht.readTemperature();
   float h = dht.readHumidity();
-  String reading = "t-" + String(t);
-  reading += ", h-" + String(h);
+  String reading = "temp-" + String(t);
+  reading += ", hum-" + String(h);
   return reading;
 }
  
@@ -39,25 +39,24 @@ void setup() {
 void loop() {
   if (millis() > lastTrans + 5000) {
     String message = readSensor();
-    Serial.println(message);
-
-    char radiopacket[20] = "Hello World #      ";
+    int n = message.length() + 1;
+    char radiopacket[n];
+    strcpy(radiopacket, message.c_str());
     Serial.println(radiopacket);
-    radiopacket[19] = 0;
     delay(10);
-    rf95.send((uint8_t *)radiopacket, 20);
+    rf95.send((uint8_t *)radiopacket, n);
     delay(10);
     rf95.waitPacketSent();
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-    if (rf95.waitAvailableTimeout(1000)) { 
-      if (rf95.recv(buf, &len)) {
-        Serial.print("Got reply: ");
-        Serial.println((char*)buf);
-        Serial.print("RSSI: ");
-        Serial.println(rf95.lastRssi(), DEC);    
-      }
-    }
+//    if (rf95.waitAvailableTimeout(1000)) { 
+//      if (rf95.recv(buf, &len)) {
+//        Serial.print("Got reply: ");
+//        Serial.println((char*)buf);
+//        Serial.print("RSSI: ");
+//        Serial.println(rf95.lastRssi(), DEC);    
+//      }
+//    }
     lastTrans = millis();
   }
 }
