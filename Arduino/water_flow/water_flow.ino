0fce036f28889dac8a2bf22f1bd8fa61447bbde7
BakerAO <baker.ousley@gmail.com>
2020-03-30 10:43:16 -0500

    Up

--- Arduino/water_flow/water_flow.ino
+++ Arduino/water_flow/water_flow.ino
@@ -1,52 +1,107 @@
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin       = 13;
-// Approximately 4.5 pulses per second per litre/minute of flow.
-float calibrationFactor = 4.5;
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+#define RF95_FREQ 915.0
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+byte sensorPin = 12;
 volatile byte pulseCount;  
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
 unsigned long oldTime;
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per litre/minute of flow.
+float calibrationFactor = 4.5;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(100);
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  rf95.init();
+  rf95.setFrequency(RF95_FREQ);
+  rf95.setTxPower(23, false);
+
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  oldTime = 0;
+
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(digitalPinToInterrupt(12), pulseCounter, FALLING);
+//  Serial.begin(115200);
 }
 
 void loop() {
-   if((millis() - oldTime) > 1000) {
-    detachInterrupt(sensorInterrupt);
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
-    unsigned int frac;
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));
-    Serial.print("L/min");
-    Serial.print("\t");
-    Serial.print("Output Liquid Quantity: ");        
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-    Serial.print("\t");
-    Serial.print(totalMilliLitres/1000);
-    Serial.print("L");
-
-    pulseCount = 0;
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  String reading = readSensor();
+  if (reading.length() > 0) {
+    sendMessage(reading);
   }
+  delay(2000);
 }
 
-void pulseCounter()
-{
+String readSensor() {
+  String reading = "";
+  if((millis() - oldTime) > 1000) {
+    detachInterrupt(digitalPinToInterrupt(12));
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTime = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+    
+    // Litres per minute
+    reading += "flowRate-";
+    reading += String(int(flowRate));
+
+    // Litres
+    reading += " totalOutput-";
+    reading += String(totalMilliLitres/1000);
+    
+    pulseCount = 0;
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(digitalPinToInterrupt(12), pulseCounter, FALLING);
+  }
+  return reading;
+}
+
+// Insterrupt Service Routine
+void pulseCounter() {
   pulseCount++;
+}
+
+void sendMessage(String message) {
+    int n = message.length() + 1;
+    char radiopacket[n];
+    strcpy(radiopacket, message.c_str());
+    delay(10);
+    rf95.send((uint8_t *)radiopacket, n);
+    delay(10);
+    rf95.waitPacketSent();
 }
