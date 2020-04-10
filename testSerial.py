ac9015e7c5be106ad51ade8a51c1641463011afa
BakerAO <dpshunter@gmail.com>
2020-04-10 08:23:58 -0500

    Update

--- testSerial.py
+++ testSerial.py
@@ -1,6 +1,6 @@
 import serial
-# import requests
-# import json
+import requests
+import json
 
 port = serial.Serial('/dev/ttyACM0', 115200)
 
@@ -8,4 +8,38 @@ while True:
   rcv = port.readline()
   if len(rcv) > 1:
     rcv = str(rcv)
-    print(rcv)
+
+    deviceIdIndex = rcv.index('deviceId-')
+    comma = rcv.index(',')
+    transmitterId = rcv[deviceIdIndex + 9 : comma]
+    rcv = rcv[comma + 1 : ]
+
+    temperatureIndex = rcv.index('temperature-')
+    if temperatureIndex > -1:
+      comma = rcv.index(',')
+      temperature = rcv[rcv.index('temperature-') + 12 : comma]
+      rcv = rcv[comma + 1 : ]
+
+    comma = rcv.index(',')
+    humidity = rcv[rcv.index('humidity-') + 9 : comma]
+    rcv = rcv[comma + 1 : ]
+
+    comma = rcv.index(',')
+    RSSI = rcv[ : comma]
+    rcv = rcv[comma + 1 : ]
+
+    SNR = rcv[ : -5]
+
+    data = {
+      'deviceId': transmitterId,
+      'temperature': temperature,
+      'humidity': humidity,
+    }
+
+    url = "https://api.innov8.host/devices/thermometers"
+    headers = {'content-type': 'application/json'}
+    try:
+      response = requests.post(url, headers=headers, data=json.dumps(data))
+      print(response)
+    except:
+      print('Bad Connection')
