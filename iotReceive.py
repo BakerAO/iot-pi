4c8f5f82ac662ecb0a0f2e1c5bbf464abe414c7c
BakerAO <baker.ousley@gmail.com>
2020-04-11 15:37:00 -0500

    Update

---
+++ iotReceive.py
@@ -0,0 +1,18 @@
+import serial
+import requests
+
+port = serial.Serial('/dev/ttyACM0', 115200)
+
+while True:
+  rcv = port.readline()
+  if len(rcv) > 1:
+    rcvString = str(rcv)
+    url = "https://api.innov8.host/devices/thermometers"
+    headers = {'content-type': 'application/json'}
+    print(rcvString)
+
+    try:
+      response = requests.post(url, headers=headers, data=rcvString)
+      print(response)
+    except:
+      print('Bad Connection')
