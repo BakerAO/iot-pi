a95e10b8f0f97a333ba926666c29eacf88dcf42a
BakerAO <baker.ousley@gmail.com>
2020-04-17 08:34:01 -0500

    Up

--- iotReceive.py
+++ iotReceive.py
@@ -2,15 +2,16 @@ import serial
 import requests
 
 port = serial.Serial('/dev/ttyACM0', 115200)
+# url = "https://api.innov8.host/devices/thermometers"
+url = "http://10.0.0.137:8081/devices/thermometers"
 
 while True:
   rcv = port.readline()
   if len(rcv) > 1:
     rcvString = str(rcv)
-    url = "https://api.innov8.host/devices/thermometers"
+    url = url
     headers = {'content-type': 'application/json'}
 
-    print(rcvString)
     try:
       response = requests.post(url, headers=headers, data=rcvString)
       print(response)
