3379e85c8ae1c4fd111f34727fe24b86040933b0
BakerAO <baker.ousley@gmail.com>
2020-05-12 10:55:24 -0500

    Update

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socket = SocketIO(app)
+
+@app.route('/')
+def home():
+  return 'Home Page'
+
+@socket.on('message')
+def handleMessage(msg):
+  print(msg)
+  send(msg)
+
+if __name__ == '__main__':
+  socket.run(app)
