d77be02befa3af9f494321efdae96a316f61c1ce
BakerAO <baker.ousley@gmail.com>
2020-05-24 11:45:43 -0500

    Update

--- python/mqttClient.py
+++ python/mqttClient.py
@@ -2,15 +2,15 @@ import paho.mqtt.client as mqtt
 
 # client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("clientId/deviceType/deviceId")
+    client.subscribe("1/water_flow/10004")
 
 # PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
