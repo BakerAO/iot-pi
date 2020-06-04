ebb080711df74a3e15d944e9fe3637bc930ab45c
BakerAO <baker.ousley@gmail.com>
2020-06-04 10:59:38 -0500

    up

--- python/iotReceive.py
+++ python/iotReceive.py
@@ -3,7 +3,7 @@ import requests
 
 port = serial.Serial('/dev/ttyACM0', 115200)
 # url = "https://api.innov8.host/devices"
-url = "http://10.0.0.137/devices"
+url = "http://10.0.0.138:8081/devices"
 
 while True:
   rcv = port.readline()
