925458386c4fd93a30348366134ac4e2efed0f05
BakerAO <baker.ousley@gmail.com>
2020-06-29 08:57:22 -0500

    up

--- Arduino/water_flow3/water_flow3.ino
+++ Arduino/water_flow3/water_flow3.ino
@@ -1,13 +1,26 @@
 #include <RH_RF95.h>
 
+#define VBAT_PIN A7 // 12
+#define INPUT_PIN 6
+#define ENABLE_PIN A3 // 17
+#define SENSOR_PIN 11
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 #define RF95_FREQ 915.0
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-int inputPin = 11;
-String deviceId = "20001";
+String deviceId = "10005";
+String valveStatus = "open";
+volatile byte pulseCount = 0;
+float flowRate = 0;
+unsigned int flowMilliLitres = 0;
+unsigned long totalMilliLitres = 0;
+unsigned long lastTrans = 0;
+unsigned long lastReading = 0;
+// The hall-effect flow sensor outputs approximately
+// 4.5 pulses per second per litre/minute of flow
+float calibrationFactor = 4.5;
 
 void setup() {
   pinMode(RFM95_RST, OUTPUT);
@@ -22,12 +35,84 @@ void setup() {
   rf95.setFrequency(RF95_FREQ);
   rf95.setTxPower(23, false);
 
-  pinMode(inputPin, OUTPUT);
+  pinMode(INPUT_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
+  digitalWrite(SENSOR_PIN, HIGH);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), pulseCounter, FALLING);
+//  Serial.begin(115200);
 }
 
 void loop() {
+  readAndSend(false);
   receiveMessage();
   delay(1000);
+}
+
+void readAndSend(bool forceSend) {
+  String reading = readSensor(forceSend);
+  if (reading.length() > 0) {
+    sendMessage(reading);
+  }
+}
+
+String readSensor(bool forceSend) {
+  String reading = "";
+  if ((millis() - lastReading) > 1000 || forceSend) {
+    detachInterrupt(digitalPinToInterrupt(SENSOR_PIN));
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - lastReading)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    lastReading = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+
+    reading += "{ \"device_id\": " + deviceId;
+    reading += ", \"battery\": " + readBattery();
+    reading += ", \"flow_rate\": " + String(flowRate); // Litres per minute
+    reading += ", \"total_output\": " + String(totalMilliLitres/1000); // Litres
+    reading += ", \"valve_status\": \"" + valveStatus + "\" }";
+
+    pulseCount = 0;
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), pulseCounter, FALLING);
+    lastTrans++;
+  }
+
+  if (forceSend || flowRate > 0 || lastTrans > 29 || (valveStatus == "closed" && lastTrans > 4)) {
+    lastTrans = 0;
+    return reading;
+  } else {
+    return "";
+  }
+}
+
+// Insterrupt Service Routine
+void pulseCounter() {
+  pulseCount++;
+}
+
+void sendMessage(String message) {
+  int n = message.length() + 1;
+  char radiopacket[n];
+  strcpy(radiopacket, message.c_str());
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, n);
+  delay(10);
+  rf95.waitPacketSent();
 }
 
 void receiveMessage() {
@@ -50,9 +135,21 @@ void processMessage(String message) {
 }
 
 void activateMotor() {
-  digitalWrite(inputPin, HIGH);
+  analogWrite(ENABLE_PIN, 10);
+  digitalWrite(INPUT_PIN, HIGH);
+  valveStatus = "closed";
 }
 
 void deactivateMotor() {
-  digitalWrite(inputPin, LOW);
+  analogWrite(ENABLE_PIN, 0);
+  digitalWrite(INPUT_PIN, LOW);
+  valveStatus = "open";
+}
+
+String readBattery() {
+  float vbat = analogRead(VBAT_PIN);
+  vbat *= 2;    // we divided by 2, so multiply back
+  vbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  vbat /= 1024; // convert to voltage
+  return String(vbat);
 }
--- python/listen.py
+++ python/listen.py
@@ -4,25 +4,20 @@ import requests
 
 broker = 'broker.innov8.host'
 port = serial.Serial('/dev/ttyACM0', 115200)
-api = 'https://api.innov8.host/devices'
-# api = 'http://10.0.0.142:8081/devices'
+# api = 'https://api.innov8.host/devices'
+api = 'http://10.0.0.142:8081/devices'
 
 # CONNACK received from the server.
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code " + str(rc))
-  client.subscribe("1/water_flow/10004")
+  client.subscribe("1/water_flow")
 
 # PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
   payload = msg.payload.decode('UTF-8')
   print(msg.topic)
   print(payload)
-  if payload == 'shut_off':
-    serialMsg = '10004-SHUT_OFF'
-    port.write(serialMsg.encode())
-  if payload == 'open':
-    serialMsg = '10004-OPEN'
-    port.write(serialMsg.encode())
+  port.write(payload.encode())
 
 client = mqtt.Client()
 client.on_connect = on_connect
