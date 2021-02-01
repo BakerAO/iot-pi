751c154ce71fc90dc53df9828264314ac76df55e
BakerAO <dpshunter@gmail.com>
2021-02-01 10:10:55 -0600

    gps update

--- Arduino/gps/gps.ino
+++ Arduino/gps/gps.ino
@@ -1,7 +1,10 @@
+#include <TinyGPS++.h>
 #include <RH_RF95.h>
-#include <Adafruit_GPS.h>
 
 #define VBAT_PIN A7 // 12
+#define INPUT_PIN A4 // 16
+#define ENABLE_PIN A3 // 17
+#define SENSOR_PIN A1 // 15
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
@@ -9,12 +12,10 @@
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 String deviceId = "10007";
-Adafruit_GPS gps(&Serial1);
-
-long lat, lon;
-unsigned long fix_age, time, date, speed, course;
-unsigned long chars;
-unsigned short sentences, failed_checksum;
+TinyGPSPlus gps;
+float lat, lon, alt;
+uint8_t sats;
+uint32_t hdop, startTime, sendTime;
 
 void setup() {
   pinMode(RFM95_RST, OUTPUT);
@@ -29,37 +30,54 @@ void setup() {
   rf95.setFrequency(RF95_FREQ);
   rf95.setTxPower(23, false);
 
-  gps.begin(9600);
-
-  // Serial1.begin(9600);
-  Serial.begin(115200);
+  Serial1.begin(9600);
+//  Serial.begin(115200);
 }
 
 void loop() {
+  gpsWaitFix();
   readAndSend();
-  // delay(10000);
 }
+
+
+void gpsWaitFix() {
+  uint8_t GPSchar;
+  startTime = millis();
+  while (1) {
+    if (Serial1.available() > 0) {
+      GPSchar = Serial1.read();
+      gps.encode(GPSchar);
+    }
+
+    if (gps.location.isUpdated() && gps.altitude.isUpdated()) {
+      sendTime = millis();
+      break;
+    }
+  }
+}
+
 
 void readAndSend() {
-  String reading = readSensor();
-  if (reading.length() > 0) {
-    sendMessage(reading);
-    Serial.println(reading);
-  }
-}
-
-String readSensor() {
   String reading = "";
-  if (gps.newNMEAreceived()) {
-      gps.parse(gps.lastNMEA());
-      lat = gps.latitudeDegrees;
-      reading += "{ \"device_id\": " + deviceId;
-      reading += ", \"battery\": " + readBattery();
-      reading += ", \"lat\": " + String(lat); 
-      reading += ", \"lng\": " + String(lon) + " }";
-  }
+  float tempHDOP;
+  int timeLength = sendTime - startTime;
+  lat = gps.location.lat();
+  lon = gps.location.lng();
+  alt = gps.altitude.meters();
+  sats = gps.satellites.value();
+  hdop = gps.hdop.value();
+  tempHDOP = ((float) hdop / 100);
 
-  return reading;
+  reading += "{ \"device_id\": " + deviceId;
+  reading += ", \"battery\": " + readBattery();
+  reading += ", \"latitude\": " + String(lat, 8);
+  reading += ", \"longitude\": " + String(lon, 8);
+  reading += ", \"satellites\": " + String(sats);
+  reading += ", \"hdop\": " + String(tempHDOP, 2);
+  reading += ", \"altitude\": \"" + String(alt, 1) + "\" }";
+
+//  Serial.println(reading);
+  sendMessage(reading);
 }
 
 void sendMessage(String message) {
