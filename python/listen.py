29317d88f9c068a9240e8ebb04529f310e78d0dd
BakerAO <dpshunter@gmail.com>
2022-07-03 14:37:26 -0500

    Update

--- python/listen.py
+++ python/listen.py
@@ -2,22 +2,24 @@ from dotenv import dotenv_values, find_dotenv # pip3 install python-dotenv
 import paho.mqtt.client as mqtt # pip3 install paho-mqtt
 import serial # pip3 install pyserial
 import requests
-import argparse
+# import argparse
 
 env = dotenv_values(find_dotenv())
+clientId = env['CLIENT_ID']
 broker = env['MQTT_BROKER']
-port = env['MQTT_PORT']
+mqttPort = env['MQTT_PORT']
 user = env['MQTT_USER']
 password = env['MQTT_PASSWORD']
-
-argParser = argparse.ArgumentParser()
-argParser.add_argument('-dev', default=False, action='store_true')
-args = argParser.parse_args()
-port = serial.Serial('/dev/ttyACM0', 115200)
 api = 'http://api.tidoba.com/devices'
-if args.dev:
-  api = 'http://10.0.0.5:8081/devices'
-  broker = '10.0.0.5'
+
+serialPort = serial.Serial('/dev/ttyACM0', 115200)
+
+# argParser = argparse.ArgumentParser()
+# argParser.add_argument('-dev', default=False, action='store_true')
+# args = argParser.parse_args()
+# if args.dev:
+#   api = 'http://10.0.0.5:8081/devices'
+#   broker = '10.0.0.5'
 print(api)
 print(broker)
 
@@ -31,17 +33,17 @@ def on_message(client, userdata, msg):
   payload = msg.payload.decode('UTF-8')
   print(msg.topic)
   print(payload)
-  port.write(payload.encode())
+  serialPort.write(payload.encode())
 
 client = mqtt.Client()
-client.username_pw_set(user, password)
+# client.username_pw_set(user, password)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(broker, port)
+client.connect(broker, mqttPort)
 client.loop_start()
 
 while True:
-  rcv = port.readline()
+  rcv = serialPort.readline()
   if len(rcv) > 1:
     try:
       rcvString = rcv.decode('UTF-8')
