de258aed9914f0fde6a935df69f322ff9e649a18
BakerAO <dpshunter@gmail.com>
2022-07-03 14:51:27 -0500

    Update

--- python/iotReceive.py
+++ python/iotReceive.py
@@ -1,20 +1,20 @@
 import serial
-import requests
+# import requests
 
 port = serial.Serial('/dev/ttyACM0', 115200)
-url = "https://api.tidoba.com/devices"
+# url = "https://api.tidoba.com/devices"
 # url = "http://10.0.0.142:8081/devices"
 
 while True:
   rcv = port.readline()
   if len(rcv) > 1:
     rcvString = str(rcv)
-    url = url
-    headers = {'content-type': 'application/json'}
+    # url = url
+    # headers = {'content-type': 'application/json'}
 
     try:
       print(rcvString)
-      response = requests.post(url, headers=headers, data=rcvString)
-      print(response)
+      # response = requests.post(url, headers=headers, data=rcvString)
+      # print(response)
     except:
       print('Bad Connection')
