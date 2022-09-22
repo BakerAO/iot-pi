3ed690a80c8b4aa4155efb288714a28f36a202fd
BakerAO <baker.ousley@gmail.com>
2022-09-22 16:48:31 -0500

    up

--- python/apiCheck.py
+++ python/apiCheck.py
@@ -5,12 +5,9 @@ from time import sleep
 env = dotenv_values(find_dotenv())
 api = 'http://' + env['API'] + '/devices'
 
-while True:
-    print('Start')
-    sleep(3)
-    try:
-        print('Send Request')
-        response = requests.get(api)
-        print(response)
-    except:
-      print('Error')
+try:
+    print('Send Request')
+    response = requests.get(api)
+    print(response)
+except:
+    print('Error')
